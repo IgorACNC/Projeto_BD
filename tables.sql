d8231f04f0ce19868b3ed89c377640b5e36ad735
Bruno <bcf2@cesar.school>
2025-09-19 19:45:32 -0300

    table tecnico

---
+++ tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Tecnico(
+    id_tecnico INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
+    quant_time_treinou INT NOT NULL
+);
+
+
+
