dd8676a910b76080c24f449beba07d7b7604cb7b
Bruno <bcf2@cesar.school>
2025-09-19 20:09:28 -0300

    organizacao

--- schema.sql
+++ schema.sql
@@ -1,9 +1,12 @@
+CREATE DATABASE IF NOT EXISTS Brasileirao_serie_A;
+USE Brasileirao_serie_A;
+
 CREATE TABLE IF NOT EXISTS Tecnico(
     id_tecnico INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     idade INT NOT NULL,
     quant_time_treinou INT NOT NULL
 );
-
+ 
 
 
