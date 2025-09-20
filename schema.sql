d6361983fe32a2bb8108389f310f554608be27c2
Bruno <bcf2@cesar.school>
2025-09-19 21:22:09 -0300

    table presidente and data

--- schema.sql
+++ schema.sql
@@ -5,8 +5,16 @@ CREATE TABLE IF NOT EXISTS Tecnico(
     id_tecnico INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     idade INT NOT NULL,
-    quant_time_treinou INT NOT NULL
+    quant_time_treinou INT NOT NULL DEFAULT 1 
 );
+CREATE TABLE IF NOT EXISTS Presidente(
+    id_presidente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
+    quant_titulo INT NOT NULL DEFAULT 0,
+    tempo_cargo INT NOT NULL DEFAULT 1
+);
+
  
 
 
