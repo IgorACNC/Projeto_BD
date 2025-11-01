590747785ad7c5df639d1ef86ca3100685c9c10e
Bruno <bcf2@cesar.school>
2025-11-01 18:52:49 -0300

    fruncao e procedimento

--- queries.sql
+++ queries.sql
@@ -130,3 +130,85 @@ FROM Time t
 LEFT JOIN Presidente p ON t.fk_presidente = p.id_presidente
 LEFT JOIN Tecnico te ON t.fk_tecnico = te.id_tecnico
 LEFT JOIN Estadio e ON t.fk_estadio = e.id_estadio;
+
+-- entrega 5
+
+-- Função para classificar desempenho do jogador com base nos gols na temporada
+DELIMITER $$
+CREATE FUNCTION fn_ClassificarDesempenho(gols INT)
+RETURNS VARCHAR(30)
+DETERMINISTIC
+BEGIN
+    DECLARE nivel VARCHAR(30);
+    
+    IF gols >= 15 THEN
+        SET nivel = 'Candidato a Artilheiro';
+    ELSEIF gols >= 5 THEN
+        SET nivel = 'Bom Desempenho';
+    ELSE
+        SET nivel = 'Desempenho Regular';
+    END IF;
+    
+    RETURN nivel;
+END$$
+DELIMITER ;
+
+-- participações em gols dos jogadores
+-- Esta função calcula o total de participações em gols (gols + assistências) de um jogador.
+DELIMITER $$
+CREATE FUNCTION fn_CalcularParticipacoesGols(gols INT, assistencias INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE total_participacoes INT;
+    SET total_participacoes = gols + assistencias;
+    RETURN total_participacoes;
+END$$
+DELIMITER ;
+
+-- procedimento atualiza o técnico de um time e incrementa o contador de times treinados do novo técnico
+DELIMITER $$
+CREATE PROCEDURE sp_ContratarTecnico(IN time_id INT, IN novo_tecnico_id INT)
+BEGIN
+    UPDATE Time
+    SET fk_tecnico = novo_tecnico_id
+    WHERE id_time = time_id;
+    
+    UPDATE Tecnico
+    SET quant_time_treinou = quant_time_treinou + 1
+    WHERE id_tecnico = novo_tecnico_id;
+END$$
+DELIMITER ;
+
+-- procedimento usa um cursor para iterar sobre os jogadores de um time e concatená-los em uma única string de saída
+DELIMITER $$
+CREATE PROCEDURE sp_GerarListaElenco(IN time_id INT, OUT lista_elenco TEXT)
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE nome_jogador VARCHAR(100);
+    DECLARE posicao_jogador VARCHAR(20);
+    
+    DECLARE cur_elenco CURSOR FOR 
+        SELECT nome, posicao_jogador 
+        FROM Jogador 
+        WHERE fk_time = time_id 
+        ORDER BY posicao_jogador, nome;
+        
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    SET lista_elenco = '';
+    
+    OPEN cur_elenco;
+    
+    loop_jogadores: LOOP
+        FETCH cur_elenco INTO nome_jogador, posicao_jogador;    
+        IF done THEN
+            LEAVE loop_jogadores;
+        END IF;
+        
+        SET lista_elenco = CONCAT(lista_elenco, posicao_jogador, ': ', nome_jogador, '\n');
+    END LOOP;
+    
+    CLOSE cur_elenco;
+END$$
+DELIMITER ;
